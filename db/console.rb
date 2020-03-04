8e6fb67e961e8657d8554527ece562b0bd110506
Ariane Holford <aribomop@gmail.com>
2020-03-04 18:47:15 +0000

    add update methods

--- db/console.rb
+++ db/console.rb
@@ -5,13 +5,17 @@ require_relative("../models/artist")
 Album.delete_all
 Artist.delete_all
 
-
-
 artist1 = Artist.new({'name' => 'Madonna'})
 artist1.save
 
+artist1.name = "Lady Gaga"
+artist1.update
+
 artist2 = Artist.new({'name' => 'GunsNRoses'})
 artist2.save
+
+artist3 = Artist.new({'name' => 'Radiohead'})
+artist3.save
 
 album1 = Album.new({'title' => 'True Blue', 'genre' => 'Pop', 'artist_id' => artist1.id})
 album1.save
@@ -19,8 +23,12 @@ album1.save
 album2 = Album.new({'title' => 'Appetite for Destruction', 'genre' => 'Hard rock', 'artist_id' => artist2.id})
 album2.save
 
-artist1.name = "Cindy"
-artist1.update
+
+album3 = Album.new({'title' => 'OK Computer', 'genre' => 'Rock', 'artist_id' => artist1.id})
+album3.save
+
+album3.title = album2.title
+album3.update
 
 binding.pry
 nil
